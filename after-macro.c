5716cca316ef92423e1bf64d84f51e3b3b4735a8
hazifer <noamtsalisher@yahoo.com>
2024-07-24 23:00:17 +0300

    not much time, starting to work on the code

--- after-macro.c
+++ after-macro.c
@@ -1,2 +1,14 @@
 #include "after-macro.h"
 
+int get_command_op_code_decimal(char *word)
+{
+	int i;
+	char *command_set[NUMBER_OF_COMMANDS] = { "mov", "cmp", "add", "sub", 
+						  "lea", "clr", "not", "inc", 
+						  "dec", "jmp", "bne", "red", 
+						  "prn", "jsr", "rts", "stop", };
+	for (i = 0; i < NUMBER_OF_COMMANDS; ++i)
+		if (!strcmp(word, command_set[i]))
+				return i;
+	return -1;
+}
